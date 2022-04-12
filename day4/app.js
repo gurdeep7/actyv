215d63e8cad52f6b34707d1d27d8e4ca30557616
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2022-04-12 15:27:50 +0530

    progress

--- day4/app.js
+++ day4/app.js
@@ -10,6 +10,7 @@ var booksRouter = require("./routes/books");
 var authRouter = require("./routes/auth")
 var app = express();
 
+const connect = require("./connections/mongoose")
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,7 +25,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
 app.use('/auth', authRouter)
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -40,5 +40,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+//Connecting to server
 
+app.listen(2345,async()=>{  // do not add localhost here if you are deploying it
+  await connect();
+  console.log("server listening to "+ 2345);
+});
 module.exports = app;
