973f17e3a795a11c1c49f1521471a584ae500361
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2022-04-12 12:19:53 +0530

    day3 done

--- day3/routes/books.js
+++ day3/routes/books.js
@@ -10,7 +10,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 router.post('/add', function (req, res, next) {
+
   console.info(req.body);
   res.status(200).json({ message:  "Book Saved Successfully" });
 });
+
+router.get("/add",function (req, res, next) {
+  res.render('bookAdd', { title: 'ADD BOOK' });
+
+  
+})
 module.exports = router;
